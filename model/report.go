57aa99f15e9b84f6858411a82096fa86ca54c39f
godcong <jumbycc@163.com>
2019-01-12 02:04:41 +0800

    add report

--- model/report.go
+++ model/report.go
@@ -4,10 +4,46 @@ import "github.com/mongodb/mongo-go-driver/bson/primitive"
 
 // Report ...
 type Report struct {
-	ID            primitive.ObjectID `bson:"_id,omitempty"`
+	Model         `bson:",inline"`
 	MediaID       primitive.ObjectID `bson:"media_id"`       //举报视频ID
 	ExoID         primitive.ObjectID `bson:"exo_id"`         //用户ID
 	Types         string             `bson:"types"`          //举报类型
 	Detail        string             `bson:"detail"`         //举报详情
 	ProcessResult string             `bson:"process_result"` //处理结果
+}
+
+// CreateIfNotExist ...
+func (r *Report) CreateIfNotExist() error {
+	return CreateIfNotExist(r)
+}
+
+// Create ...
+func (r *Report) Create() error {
+	return InsertOne(r)
+}
+
+// Update ...
+func (r *Report) Update() error {
+	return UpdateOne(r)
+}
+
+// Delete ...
+func (r *Report) Delete() error {
+	return DeleteByID(r)
+}
+
+// Find ...
+func (r *Report) Find() error {
+	return FindByID(r)
+}
+
+func (r *Report) _Name() string {
+	return "report"
+}
+
+// NewReport ...
+func NewReport() *Report {
+	return &Report{
+		Model: model(),
+	}
 }
--- service/user_controller.go
+++ service/user_controller.go
@@ -1,10 +1,30 @@
 package service
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/godcong/role-manager-server/model"
+)
 
 // UserReport ...
 func UserReport(ver string) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		report := model.NewReport()
+
+		eid := ctx.PostForm("exo_id")
+		if eid == "" {
+			failed(ctx, "null exo_id")
+		}
+		report.ExoID = model.ID(eid)
+		mid := ctx.PostForm("media_id")
+		if mid == "" {
+			failed(ctx, "null media_id")
+		}
+		report.MediaID = model.ID(mid)
+		report.Types = ctx.PostForm("types")
+		report.Detail = ctx.PostForm("detail")
+		report.ProcessResult = "commit"
+
+		report.Create()
 		return
 	}
 }
